bf4b16adda08c89ebd076ee2366774b943b855d5
GilHyeonwoo <xlsxls753@naver.com>
2025-01-23 18:37:38 +0900

    계산기와 Junit5를 활용한 테스트코드 문자열 계산기와 테스트코드

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,36 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    void testAdd(){
+        Calculator calculator = new Calculator();
+        int result = calculator.add(5,3);
+        assertEquals(8,result,"예상결과 8");
+    }
+    @Test
+    void testSub(){
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(5,3);
+        assertEquals(2,result,"예상결과 2");
+    }
+    @Test
+    void testdivide(){
+        Calculator calculator = new Calculator();
+        int result = calculator.divide(5,5);
+        assertEquals(1,result,"예상결과 1");
+    }
+    @Test
+    void testmultiply(){
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(5,3);
+        assertEquals(15,result,"예상결과 15");
+    }
+    @Test
+    void testmultiplyzero(){
+        Calculator calculator = new Calculator();
+        assertThrows(IllegalArgumentException.class,() -> calculator.divide(10,0),"0으로 나누면 오류");
+    }
+}
